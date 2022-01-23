94f81fe54bb1b2ea47f801f752aa7867fa35032a
MilhouseVanProuten <milhousevanprouten@gmail.com>
2022-01-23 20:03:16 +0100

    cleanup

--- server.py
+++ server.py
@@ -2,22 +2,16 @@ import json
 from flask import Flask, render_template, request, redirect, flash, url_for
 from datetime import datetime as dt
 
-date = dt.now()
 
 def loadClubs():
     file_path = 'clubs.json'
-    if app.config['TESTING']:
-        file_path = '/Users/nicolasfrenay/Desktop/Formation/P11_amelioration_application_web/Python_Testing/clubs.json'
     with open(file_path) as c:
         listOfClubs = json.load(c)['clubs']
-        # print(app.config)
         return listOfClubs
 
 
 def loadCompetitions():
     file_path = 'competitions.json'
-    if app.config['TESTING']:
-        file_path = '/Users/nicolasfrenay/Desktop/Formation/P11_amelioration_application_web/Python_Testing/competitions.json'
     with open(file_path) as comps:
         listOfCompetitions = json.load(comps)['competitions']
         return listOfCompetitions
@@ -33,11 +27,13 @@ past_competitions = []
 competitions = []
 
 # loop to separate past competitions
+date = dt.now()
 for comp in all_competitions:
-    if dt.strptime(comp['date'],'%Y-%m-%d %H:%M:%S') < date:
+    if dt.strptime(comp['date'], '%Y-%m-%d %H:%M:%S') < date:
         past_competitions.append(comp)
     else:
         competitions.append(comp)
+
 
 @app.route('/')
 def index():
@@ -49,8 +45,7 @@ def showSummary():
     # add a try/except to avoid index error with unknown email
     try:
         club = \
-            [club for club in clubs if club['email'] == request.form['email']][
-                0]
+        [club for club in clubs if club['email'] == request.form['email']][0]
     except IndexError:
         error = True
         return render_template('index.html', error=error)
@@ -65,8 +60,8 @@ def book(competition, club):
     # add try/except to avoid indexerror with unknown club or competition
     try:
         foundClub = [c for c in clubs if c['name'] == club][0]
-        foundCompetition = \
-            [c for c in competitions if c['name'] == competition][0]
+        foundCompetition = [c for c in competitions if c['name']
+                            == competition][0]
         return render_template('booking.html', club=foundClub,
                                competition=foundCompetition)
     except IndexError:
@@ -79,8 +74,8 @@ def book(competition, club):
 
 @app.route('/purchasePlaces', methods=['POST'])
 def purchasePlaces():
-    competition = \
-        [c for c in competitions if c['name'] == request.form['competition']][0]
+    competition = [c for c in competitions if c['name']
+                   == request.form['competition']][0]
     club = [c for c in clubs if c['name'] == request.form['club']][0]
     placesRequired = int(request.form['places'])
 
@@ -93,7 +88,7 @@ def purchasePlaces():
                                club_list=clubs)
 
     # check if club as enough points
-    if int(club['points']) < placesRequired*3:
+    if int(club['points']) < placesRequired * 3:
         flash("Vous ne disposez pas d'assez de points !")
         return render_template('welcome.html', club=club,
                                competitions=competitions,
@@ -111,15 +106,12 @@ def purchasePlaces():
         competition['numberOfPlaces'] = int(
             competition['numberOfPlaces']) - placesRequired
         # Fix club point update
-        club['points'] = int(club['points']) - placesRequired*3
+        club['points'] = int(club['points']) - placesRequired * 3
         flash('Great-booking complete!')
         return render_template('welcome.html', club=club,
                                competitions=competitions,
                                past_competitions=past_competitions,
                                club_list=clubs)
-
-
-# TODO: Add route for points display
 
 
 @app.route('/logout')
